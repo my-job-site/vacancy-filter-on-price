4807e38129238fccfcedb8e685f2787bbc3f6c20
w0rng <antonabramov2000@gmail.com>
2022-05-20 15:15:38 +0700

    add logics

---
+++ src/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST"])
+def hello_world():
+    json = request.get_json()
+    user_price = float(json["resume"]["price"])
+    vacancies = json["vacancies"]
+    result = []
+    for vacancie in vacancies:
+        coef = float(vacancie["price"])/user_price
+        result.append({"id": vacancie["id"], "score": coef if 0.8 <= coef <= 1.2 else 0})
+    print(result, flush=True)
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", port=5001, debug=True)
